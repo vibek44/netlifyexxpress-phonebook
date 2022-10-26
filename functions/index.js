aa3bfd6d6f5baa3023c7866b911f91b1fb7341aa
vibek44 <kshetrivibek@gmail.com>
2022-10-26 21:15:57 +0300

    adding function needed for netlify

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+
+const serverless=require('serverless-http')
+const express=require('express')
+const  morgan=require('morgan')
+const router=require('../routes/router')
+
+const app=express()
+
+app.use(express.json())
+
+morgan.token('body',function(req,res){
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use('/', router)
+
+
+
+
+module.exports.handler=serverless(app)
