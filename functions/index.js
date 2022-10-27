10614d582d7d58874d9aa706cac937bbfc2363b5
vibek44 <kshetrivibek@gmail.com>
2022-10-27 18:28:22 +0300

    using build file for frontend

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ morgan.token('body',function(req,res){
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.static)
 app.use('/', router)
 
 
