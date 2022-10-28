49eef9aacb91ed0f44c992fe4d0a52806b23684e
vibek44 <kshetrivibek@gmail.com>
2022-10-28 21:46:21 +0300

    adding routepath in array  for app

--- functions/index.js
+++ functions/index.js
@@ -6,17 +6,18 @@ const cors=require('cors')
 const router=require('../routes/router')
 
 const app=express()
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+
 
 morgan.token('body',function(req,res){
     return JSON.stringify(req.body)
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use('/', router)
+app.use(['/api/persons/','/info'], router)
 
 
 
